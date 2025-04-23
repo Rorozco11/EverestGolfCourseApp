edc398b487ea8335d07e5b8040f82860d2bb0c0d
Rorozco11 <96450960+Rorozco11@users.noreply.github.com>
2025-04-23 19:46:25 -0400

    Commit

--- everest-golf-course/src/app/page.tsx
+++ everest-golf-course/src/app/page.tsx
@@ -2,14 +2,28 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="h-4 relative">
-      <Image
-        src="/Images/home.jpg"
-        alt="Home background"
-        fill
-        className= "object-top"
-        priority/>
-
+    <main>
+      {/* <div className="relative w-screen h-[75vh] z-0">
+        <Image
+          src="/Images/home.jpg"
+          alt="Home background"
+          fill
+          className="object-cover"
+          sizes="100vw"
+        />
+      </div> */}
+      
+      {/* #region Navbar */}
+      <nav className="fixed top-0 w-full p-4 bg-black/80 backdrop-blur-sm z-10">
+        <div className="flex justify-center items-center">
+          <div className="flex gap-8">
+            <a href="#" className="">Pacific Northwest</a>
+            <a href="#" className="">Southwest</a>
+            <a href="#" className="">Midwest</a>
+          </div>
+        </div>
+      </nav>
+      {/* #endregion */}
     </main>
   );
-} 
+}
